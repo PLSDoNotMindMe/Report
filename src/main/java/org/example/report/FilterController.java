0ba04090e151b3d2d551b53b67e2b784810821ec
Ilya <ibuvichevs@gmail.com>
2024-01-05 17:02:52 +0300

    Ilya

--- src/main/java/org/example/report/FilterController.java
+++ src/main/java/org/example/report/FilterController.java
@@ -8,46 +8,50 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
 
 import java.io.File;
 
 public class FilterController {
 
-String nameFile;
+    String nameFile;
+    String user;
 
     @FXML
     private TextField NameIn;
 
     @FXML
-    void name (MouseEvent event) {
+    void name(MouseEvent event) {
         nameFile = NameIn.getText();
         System.out.println("TextDone");
     }
 
     @FXML
     void createFolder(MouseEvent event) {
+        user = System.getProperty("user.name");
 
-        File f = new File("C:\\Users\\SerPivas\\Desktop\\Ошибки");
-        try{
-            if(f.mkdir()) {
+        File f = new File("C:\\Users\\" + user + "\\Desktop\\Ошибки");
+        try {
+            if (f.mkdir()) {
                 System.out.println("Directory Created");
             } else {
                 System.out.println("Directory is not created");
             }
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     @FXML
     void Error308(MouseEvent event) {
 
         //Создание документа, установка автофильтра
         Workbook wb = new Workbook();
-        wb.loadFromFile("C:\\Users\\SerPivas\\Downloads\\" + nameFile+ ".xlsx");
+        wb.loadFromFile("C:\\Users\\SerPivas\\Downloads\\" + nameFile + ".xlsx");
         Worksheet sheet = wb.getWorksheets().get(0);
         sheet.setName("Сток");
         AutoFiltersCollection filters = sheet.getAutoFilters();
-        filters.setRange(sheet.getCellRange(1,1,20762,34));
+        filters.setRange(sheet.getCellRange(1, 1, 20762, 34));
 
         //ПРИМЕНЕНИЕ ФИЛЬТРОВ 308 ОШИБКИ:
         //Фильтр колонки "Статус"
@@ -64,16 +68,12 @@ String nameFile;
         filters.addFilter(10, "Шут");
         //Фильтр колонки "Дата прихода"
         java.time.LocalDate current_date = java.time.LocalDate.now().minusDays(1);
-        filters.addDateFilter(12, DateTimeGroupingType.Day, current_date.getYear(), current_date.getMonthValue(), current_date.getDayOfMonth(), 0,0,0);
+        filters.addDateFilter(12, DateTimeGroupingType.Day, current_date.getYear(), current_date.getMonthValue(), current_date.getDayOfMonth(), 0, 0, 0);
         //Фильтр колонки "В перевозке"
         filters.addFilter(27, "Нет");
         filters.filter();
-        wb.saveToFile("C:\\Users\\SerPivas\\Desktop\\Ошибки\\308.xlsx");
-
-
+        wb.saveToFile("C:\\Users\\" + user + "\\Desktop\\Ошибки\\308.xlsx");
     }
 
-
-    }
-
+}
 
--- src/main/java/org/example/report/Test.java
+++ src/main/java/org/example/report/Test.java
@@ -1,19 +1,12 @@
 package org.example.report;
 
 import java.io.File;
+import java.net.InetAddress;
 
 public class Test {
+
     public static void main(String[] args) {
-        File f = new File("C:\\Users\\SerPivas\\Desktop\\Ошибки");
-        try{
-            if(f.mkdir()) {
-                System.out.println("Directory Created");
-            } else {
-                System.out.println("Directory is not created");
-            }
-        } catch(Exception e){
-            e.printStackTrace();
-        }
+
     }
 
 }
